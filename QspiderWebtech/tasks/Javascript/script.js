39dbc01dc9363c4a3924db1bce849636b6b2a9cd
Sanjay <smohanta580@gmail.com>
2025-10-16 17:37:22 +0530

    Refactor HTML structure and enhance JavaScript functionality; add Swiggy-themed webpage with CSS styling

--- QspiderWebtech/tasks/Javascript/script.js
+++ QspiderWebtech/tasks/Javascript/script.js
@@ -1,6 +1,20 @@
-let str = "Sanjay Kumar";
-console.log(str.slice(0,5));
+// let str = "Sanjay Kumar";
+// console.log(str.slice(0,5));
 
-let arr = [1,2,3,4,5];
-console.log(arr.slice(2,4));
+// let arr = [1,2,3,4,5];
+// console.log(arr.slice(2,4));
 
+// a = 10;
+// b = 20;
+// console.log(a + b);
+
+document.writeln("Hello Sanjay");
+document.writeln("Hello Sanjay");
+document.write("hii");
+document.write("hii");
+
+console.log("Hello Sanjay");
+// console.log(object)
+
+var _a = 10;
+console.log(a);
